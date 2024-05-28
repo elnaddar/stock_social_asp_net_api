6f095fcf5f2d0abdeb08722263eaae2d6efc671a
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-05-28 17:54:12 +0300

    created Stock Controller that has two actions one for GetAll another for GetById.

---
+++ api/Controllers/StockController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public StockController(ApplicationDbContext dbContext)
+        {
+            _context = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var stocks = _context.Stocks.ToList();
+            return Ok(stocks);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            var stock = _context.Stocks.Find(id);
+            if (stock == null)
+            {
+                return NotFound();
+            }
+            return Ok(stock);
+        }
+    }
+}
