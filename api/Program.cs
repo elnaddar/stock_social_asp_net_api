c09b0ad149983034bd7423541fa956c6c7961fb1
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-05-28 17:17:55 +0300

    configured EF core.

--- api/Program.cs
+++ api/Program.cs
@@ -1,9 +1,20 @@
+using api.Data;
+using DotNetEnv;
+using Microsoft.EntityFrameworkCore;
+
+Env.Load();
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    var connectionString = Environment.GetEnvironmentVariable("DefaultConnection");
+    options.UseSqlServer(connectionString);
+});
 
 var app = builder.Build();
 
