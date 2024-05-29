4fbd64f3747446300f0e06ce3d31bbb184741b83
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-05-29 11:10:36 +0300

    created action for getAll.

--- api/Controllers/CommentController.cs
+++ api/Controllers/CommentController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Interfaces;
+using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
@@ -12,5 +13,13 @@ namespace api.Controllers
     public class CommentController(ICommentRepository repo) : ControllerBase
     {
         private readonly ICommentRepository _repo = repo;
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var comments = await _repo.GetAllAsync();
+            var commentsDto = comments.Select(c => c.ToCommentDto());
+            return Ok(comments);
+        }
     }
 }
