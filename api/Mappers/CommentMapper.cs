62bdf07be6fb5729e0fd3d7097e3fda32ac6b5ac
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-05-29 11:10:14 +0300

    created CommentDto and its mapper.

---
+++ api/Mappers/CommentMapper.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Dtos.Comment;
+
+namespace api.Mappers
+{
+    public static class CommentMapper
+    {
+        public static CommentDto ToCommentDto(this Models.Comment comment)
+        {
+            return new()
+            {
+                Id = comment.Id,
+                Title = comment.Title,
+                Content = comment.Content,
+                CreatedOn = comment.CreatedOn,
+                StockId = comment.StockId,
+                Stock = comment.Stock,
+            };
+        }
+    }
+}
