36c073687f761a6b162491f8a2396a084bc42994
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-05-29 17:46:54 +0300

    added validations for comment dtos.

--- api/Dtos/Comment/CommentCreateDto.cs
+++ api/Dtos/Comment/CommentCreateDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,14 @@ namespace api.Dtos.Comment
 {
     public class CommentCreateDto
     {
+        [Required]
+        [MinLength(10, ErrorMessage = "Title length can't be less than 10 charachters.")]
+        [MaxLength(280, ErrorMessage = "Title length can't be over 280 charachters.")]
         public string Title { get; set; } = string.Empty;
+        
+        [Required]
+        [MinLength(50, ErrorMessage = "Content length can't be less than 50 charachters.")]
+        [MaxLength(1000, ErrorMessage = "Content length can't be over 1000 charachters.")]
         public string Content { get; set; } = string.Empty;
     }
 }
