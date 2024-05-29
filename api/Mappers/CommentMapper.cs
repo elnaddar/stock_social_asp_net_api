e90adf41b0fd7c4da67d9bc9cf9a860cb800a71d
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-05-29 12:44:43 +0300

    created a comment dto for post request and its mapper.

--- api/Mappers/CommentMapper.cs
+++ api/Mappers/CommentMapper.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Dtos.Comment;
+using api.Models;
 
 namespace api.Mappers
 {
     public static class CommentMapper
     {
-        public static CommentDto ToCommentDto(this Models.Comment comment)
+        public static CommentDto ToCommentDto(this Comment comment)
         {
             return new()
             {
@@ -17,6 +18,15 @@ namespace api.Mappers
                 Content = comment.Content,
                 CreatedOn = comment.CreatedOn,
                 StockId = comment.StockId,
+            };
+        }
+        public static Comment ToComment(this CommentCreateDto commentDto, int stockId)
+        {
+            return new()
+            {
+                Title = commentDto.Title,
+                Content = commentDto.Content,
+                StockId = stockId,
             };
         }
     }
