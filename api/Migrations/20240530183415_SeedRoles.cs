c6cd619e748c4f8f7200f92e2d9ef07b35ff1798
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-05-31 18:49:45 +0300

    added migrations for roles then updated the database.

---
+++ api/Migrations/20240530183415_SeedRoles.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "2e63c10b-56c8-4c7f-bb80-5595b9e8f4e2", null, "Admin", "ADMIN" },
+                    { "61bcedea-678a-4a56-bc67-cbe21f085c21", null, "User", "USER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2e63c10b-56c8-4c7f-bb80-5595b9e8f4e2");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "61bcedea-678a-4a56-bc67-cbe21f085c21");
+        }
+    }
+}
