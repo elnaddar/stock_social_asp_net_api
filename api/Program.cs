adee1b4333de9d2f688f163956ae81eb64355877
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-05-31 19:11:15 +0300

    added token service to dependency injection.

--- api/Program.cs
+++ api/Program.cs
@@ -2,6 +2,7 @@ using api.Data;
 using api.Interfaces;
 using api.Models;
 using api.Repositories;
+using api.Services;
 using DotNetEnv;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
@@ -64,6 +65,7 @@ builder.Services.AddAuthentication(options =>
 
 builder.Services.AddScoped<IStockRepository, StockRepository>();
 builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+builder.Services.AddScoped<ITokenService, TokenService>();
 
 var app = builder.Build();
 
