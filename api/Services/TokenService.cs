8774448a9b0970c9b640b39b4f315232bc4239c7
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-05-31 19:10:54 +0300

    created token service.

---
+++ api/Services/TokenService.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using api.Interfaces;
+using api.Models;
+using Microsoft.IdentityModel.Tokens;
+
+namespace api.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly SymmetricSecurityKey _key = new(Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("JWT_ISSUER_SIGNING_KEY")!));
+        public string CreateToken(User user)
+        {
+            List<Claim> claims = [
+                new(JwtRegisteredClaimNames.Email, user.Email!),
+                new(JwtRegisteredClaimNames.GivenName, user.UserName!)
+            ];
+
+            SigningCredentials creds = new(_key, SecurityAlgorithms.HmacSha512Signature);
+            SecurityTokenDescriptor tokenDescriptor = new()
+            {
+                Subject = new(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = creds,
+                Issuer = Environment.GetEnvironmentVariable("JWT_ISSUER_SIGNING_KEY")!,
+                Audience = Environment.GetEnvironmentVariable("JWT_AUDIENCE")!
+            };
+
+            JwtSecurityTokenHandler tokenHandler = new();
+            SecurityToken token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
