2ce421d97343bd7a02e145443db5591678be7db0
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-05-31 18:49:20 +0300

    created roles.

--- api/Data/ApplicationDbContext.cs
+++ api/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,5 +18,21 @@ namespace api.Data
 
         public DbSet<Stock> Stocks { get; set; }
         public DbSet<Comment> Comments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            List<IdentityRole> roles = [
+                new(){
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+                },
+                new(){
+                    Name = "User",
+                    NormalizedName = "USER"
+                }
+            ];
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
     }
 }
