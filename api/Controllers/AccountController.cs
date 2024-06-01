75f464237989395dc38c7b880d1b4fe685b5f49b
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-06-01 06:59:33 +0300

    made login action

--- api/Controllers/AccountController.cs
+++ api/Controllers/AccountController.cs
@@ -8,15 +8,33 @@ using api.Mappers;
 using api.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class AccountController(UserManager<User> userManager, ITokenService tokenService) : ControllerBase
+    public class AccountController(UserManager<User> userManager, ITokenService tokenService, SignInManager<User> signInManager) : ControllerBase
     {
         private readonly UserManager<User> _userManager = userManager;
         private readonly ITokenService _tokenService = tokenService;
+        private readonly SignInManager<User> _signInManager = signInManager;
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserLoginDto userLogin)
+        {
+            var user = await _userManager.Users.FirstOrDefaultAsync(u => u.UserName == userLogin.UserName.ToLower());
+            if (user is null)
+            {
+                return Unauthorized("Wrong username or password.");
+            }
+            var result = await _signInManager.CheckPasswordSignInAsync(user, userLogin.Password, false);
+            if (!result.Succeeded)
+            {
+                return Unauthorized("Wrong username or password.");
+            }
+            return Ok(user.ToNewUserDto(_tokenService));
+        }
 
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] UserRegisterDto userRegister)
